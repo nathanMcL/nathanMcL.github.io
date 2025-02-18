97d4c534b0698db120a5b17cea00def441191752
Nathaniel Mclaughlin <Nmclaughlin296@gmail.com>
2025-02-18 21:39:21 +0000

    update about files: css, js

--- aboutScript.js
+++ aboutScript.js
@@ -23,3 +23,16 @@ document.addEventListener("DOMContentLoaded", function () {
 
     setInterval(createPacket, 800); // Generate new packets
 });
+
+// Marquee JScript
+const root = document.documentElement;
+const marqueeElementsDisplayed = getComputedStyle(root).getPropertyValue("--marquee-elements-displayed");
+const marqueeContent = document.querySelector("ul.marquee-content");
+
+root.style.setProperty("--marquee-elements", marqueeContent.children.length);
+
+for (let i = 0; i < marqueeElementsDisplayed; i++) {
+    marqueeContent.appendChild(marqueeContent.children[i].cloneNode(true));
+}
+// End of Marquee JScript
+
