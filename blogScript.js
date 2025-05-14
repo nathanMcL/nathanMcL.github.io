3f2ff76d67a605635d342a555f8a64e59b68e7e9
Nathaniel Mclaughlin <Nmclaughlin296@gmail.com>
2025-05-14 17:33:00 +0000

    Attempt to conform the blog content to the updated CSP

--- blogScript.js
+++ blogScript.js
@@ -3,11 +3,16 @@ document.addEventListener("DOMContentLoaded", function () {
     fetch('https://raw.githubusercontent.com/nathanMcL/nathanMcL.github.io/main/ReadMe.md')
         .then(response => response.text())
         .then(markdown => {
-            // Once the markdown is fetched, we convert it to HTML using marked.js
-            document.getElementById('markdown-content').innerHTML = marked.parse(markdown);
+            // Once the markdown is fetched, we parse it to HTML using marked.js.
+            // Because the need to improve security of the digital content it is also important update the code to meet the updated CSP.
+            // Refering to the Policy for the CSP.
+            // Purify the DOM by sanitizing the markdown content.
+            const policy = DOMPurify.sanitize(marked.parse(markdown)); 
+            document.getElementById('markdown-content').innerHTML = policy;
         })
         .catch(err => {
             console.error('Error fetching markdown:', err);
             document.getElementById('markdown-content').innerHTML = 'Failed to load content.';
         });
 });
+
