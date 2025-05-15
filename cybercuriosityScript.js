e3a78aeeb3270b4927d3f5431e3880ca431cbd75
Nathaniel Mclaughlin <Nmclaughlin296@gmail.com>
2025-05-15 23:38:09 +0000

    Created a page for my amazing cyber insights... lawd. updated the other pages, i think

---
+++ cybercuriosityScript.js
@@ -0,0 +1,20 @@
+// Using Fetch to render the markdown file from my GitHub repository
+document.addEventListener("DOMContentLoaded", function () {
+    fetch('https://raw.githubusercontent.com/nathanMcL/nathanMcL.github.io/main/CSP_Readme.md')
+        .then(response => response.text())
+        .then(markdown => {
+            // Once the markdown is fetched, we parse it to HTML using marked.js
+            // Because the need to harden the digital content it is also important update the code to meet the updated CSP.
+            // Refering to the Policy for the CSP.
+            // Purify the DOM by sanitizing the markdown content.
+            const rawHtml = marked.parse(markdown);
+            const cleanHtml = DOMPurify.sanitize(rawHtml, {RETURN_TRUSTED_TYPE: true}); // TrustedHTML
+            const container = document.getElementById('markdown-content');
+            container.innerHTML = cleanHtml; // safe assignment under Trusted Types
+        })
+        .catch(err => {
+            console.error('Error fetching markdown:', err);
+            document.getElementById('markdown-content').textContent = 'Failed to load content.';
+        });
+});
+
