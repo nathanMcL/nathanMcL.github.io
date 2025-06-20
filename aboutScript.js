0f2275f119bea222182a7c8ba95dc283f38365d7
Nathaniel Mclaughlin <Nmclaughlin296@gmail.com>
2025-06-20 21:33:38 +0000

    update fetches endpoint url

--- aboutScript.js
+++ aboutScript.js
@@ -237,7 +237,7 @@ document.addEventListener("DOMContentLoaded", function () {
             }
 
             try {
-                const response = await fetch("https://macn-about-api-djgzf3csevd3ewer.northeurope-01.azurewebsites.net/generate-aboutOrb", {
+                const response = await fetch("https://macn-about-api.azurewebsites.net/generate-aboutOrb", {
                     method: "POST",
                     headers: { "Content-Type": "application/json" },
                     body: JSON.stringify({ context: "Use a tone that blends confidence, fun, and curiosity. Mention my creative writing or tech projects or military background, and passion for equitable code and accessibility. Intended for the MacN_iT About page." })
